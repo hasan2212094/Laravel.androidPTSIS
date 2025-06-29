c34db207a1488d5335139833e962158cb72ffd78
Rodina Vilandy <vilandy.rodina@gmail.com>
2025-06-29 11:36:33 +0700

    perbaikan gambar tidak muncul di quality view

--- app/Http/Resources/QualityResource.php
+++ app/Http/Resources/QualityResource.php
@@ -30,7 +30,8 @@ class QualityResource extends JsonResource
             'description' => $this->description,
             'responds' => $this->responds,
             'image_urls' => collect($this->images)
-                ->map(fn($img) => asset('storage/' . $img->image_path))
+                //->map(fn($img) => asset('storage/' . $img->image_path))
+                ->map(fn($img) => $img->image_path)
                 ->values()
                 ->all(),
             'date' => $this->date,
